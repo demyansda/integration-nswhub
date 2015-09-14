75dcadaf32dda166bafa4979a17c72a6d70ef7d7
demyansda <dmitriy@rpmsoftware.com>
2015-09-14 13:11:01 -0600

    Accessing the  env

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var config = require('integration-common/util').readConfig('RPM_CONFIG', 'config
 config.port = process.env.PORT || config.port;
 
 require('integration-web/lib').startJsonPostServer(config, function (req, res) {
-    console.log('Request headers',req.headers);
-    console.log('Request body',req.body);
+    console.info('Request headers',req.headers);
+    console.info('Request body',req.body);
     res.contentType = 'plain/text';
     res.send();
 });
