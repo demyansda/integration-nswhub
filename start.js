264f146e541f99c66797cff47dac4911c3c76c27
demyansda <dmitriy@rpmsoftware.com>
2015-09-16 14:24:25 -0600

    -

--- start.js
+++ start.js
@@ -1,13 +1,6 @@
 /* global process */
 var config = require('integration-common/util').readConfig('RPM_CONFIG', 'config.json');
 
-if (process.env.PORT) {
-    config = {
-        port: process.env.PORT,
-        path: config.path
-    };
-}
-console.log(config);
 require('integration-web/lib').startJsonPostServer(config, function (req, res) {
     console.info('Request headers', req.headers);
     console.info('Request body', req.body);
