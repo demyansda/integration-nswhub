f8db68395cc2d41269a72cf79062704bc7bb08b3
demyansda <dmitriy@rpmsoftware.com>
2015-09-16 14:35:10 -0600

    -

--- start.js
+++ start.js
@@ -5,5 +5,5 @@ require('integration-web/lib').startJsonPostServer(config, function (req, res) {
     console.info('Request headers', req.headers);
     console.info('Request body', req.body);
     res.contentType = 'plain/text';
-    res.status(200).send('OKEY');
+    res.status(200).send('OK');
 });
