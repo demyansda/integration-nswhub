dd198c616e7657f3c8c2a7a4560139ac44a7c680
demyansda <dmitriy@rpmsoftware.com>
2015-09-16 11:14:26 -0600

    Experimenting with https for heroku

---
+++ start.js
@@ -0,0 +1,16 @@
+/* global process */
+var config = require('integration-common/util').readConfig('RPM_CONFIG', 'config.json');
+
+if (process.env.PORT) {
+    config = {
+        port: process.env.PORT,
+        path: config.path
+    };
+}
+
+require('integration-web/lib').startJsonPostServer(config, function (req, res) {
+    console.info('Request headers', req.headers);
+    console.info('Request body', req.body);
+    res.contentType = 'plain/text';
+    res.status(200).send('OK');
+});
