8af21805f1fc2b636c9e224a8dcd2cb748230b5d
demyansda <dmitriy@rpmsoftware.com>
2015-09-16 12:54:22 -0600

    Experimenting with https for heroku

--- web.js
+++ web.js
@@ -1,12 +1,18 @@
 /* global process */
+console.log(process.env);
+
 var express = require('express');
 var app = express();
 
+
+
 app.post('/wh', function (request, response) {
+    console.log(request);
     response.send('Hello World!');
 });
 
 app.get('/wh', function (request, response) {
+    console.log(request);
     response.send('Hello World!');
 });
 
