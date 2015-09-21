a0ce03816fa0e40b3a4839e8001cd8c83465d3c2
Dmitriy Serdyukov <dmitriy@rpmsoftware.com>
2015-09-21 09:11:21 -0600

    Heroku test

--- client.js
+++ client.js
@@ -2,19 +2,17 @@
 var webhooks = require('integration-web/webhooks');
 var rpmUtil = require('integration-common/util');
 var util = require('util');
-var config = rpmUtil.readConfig(undefined, 'config.json');
+var config = rpmUtil.readConfig(undefined, 'config.json').webHook;
 var host = 'localhost:' + config.port;
-host = 'whtest.herokuapp.com';
-var cpath = util.format('http://%s/%s',host, config.path);
+// host = 'whtest.herokuapp.com';
+var cpath = util.format('https://%s/%s',host, config.path);
 console.log(cpath);
 var RESTClient = require('node-rest-client').Client;
 
 function send(headers, data) {
-    new RESTClient().post(cpath, { headers: headers, data: data }, function (data, response) {
-        console.log('data',data.toString());
-        // console.log(response);
-    });
+    new RESTClient().post(cpath, { headers: headers, data: data }, function (data, response) {});
 }
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
-send(new webhooks.WebHooksRequestHeader('telco', 100), new webhooks.WebHooksRequestData(100, 12, 'form.start'));
+var data = new webhooks.WebHooksRequestData(100, 12, 'form.start');
+send(new webhooks.WebHooksRequestHeader(123, 100, data, config.signSecret), data);
--- start.js
+++
@@ -1,9 +0,0 @@
-/* global process */
-var config = require('integration-common/util').readConfig('RPM_CONFIG', 'config.json');
-
-require('integration-web/lib').startJsonPostServer(config, function (req, res) {
-    console.info('Request headers', req.headers);
-    console.info('Request body', req.body);
-    res.contentType = 'plain/text';
-    res.status(200).send('OK');
-});
